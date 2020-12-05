650cdfea7da297dd7191556aa0540447325d1992
Hambaka <readeryoshi@gmail.com>
2020-12-05 11:55:50 +0800

    Add source file, update README.

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # golden_sun_password_converter
+
+![Rust](https://img.shields.io/badge/language-Rust-blueviolet.svg?style=flat-square)
+
 Golden Sun password converter (English -> Japanese, Japanese -> English)
+
+## What's this?
+
+A really simple tool for two Game Boy Advance games, Golden Sun (黄金の太陽 開かれし封印) and Golden Sun: The Lost Age (黄金の太陽 失われし時代).
+If you clear Golden Sun, you can get password of your clear data, and you can transfer your game data to Golden Sun: The Lost Age by inputting password.
+Japanese version password and English version password ***looks different***, but they are ***actually identical***. Sometimes maybe you want to convert your password to another version because of some reasons.
+
+
+## Usage
+
+Put the binary file and a text file called `input.txt` that contains Golden Sun password in the same folder, then run binary. It will output `output.txt` in the same folder.
+
+**Note: The encoding of `input.txt` must be `UTF-8`.**
+
+## Example
+
+Example `input.txt` (English version password as source)
+
+```
+w$wZZ P!Vm#
+Qndtz qKjF=
+D2YnP J2RkJ
+baHyQ ZPTqe
+HSYq! XD6%j
+
+wBMt% SmYzM
+Y?Mcp tXtsX
+W2Y6c !yxJa
+ZB&PP JCJVz
+TgFcK aN9nv
+
++yPHE TGcpz
+JSb5d RfGz7
+V$6ba Yy6LD
+H&zb8 9j3Dt
+VA8p? pD$#C
+
+22kAw zVkTJ
+$8rYb bv4EZ
+b!9Kf $dPjS
+BiRqp 5yUKE
+t5zQx 9&U?a
+
+eA3c+ EV#6r
+KaHtP eMxT!
+jS#X% W+YfZ
+bD9y7 Hd#bS
+Ni=gT XPxW#
+
+tJr3x Md7uJ
+```
+
+`output.txt` (result is Japanese version password)
+
+```
+じびじねね せだとわど
+そんやげぞ がこれかぼ
+えのぬんせ けのたろけ
+めむくぜそ ねせつがゆ
+くちぬがだ にえへぶれ
+
+じいしげぶ ちわぬぞし
+ぬでしもを げにげぐに
+なのぬへも だぜずけむ
+ねいばせせ けうけとぞ
+つらかもこ むすみんざ
+
+べぜせくお つきもをぞ
+けちめふや たよきぞほ
+とびへめむ ぬぜへさえ
+くばぞめま みれはえげ
+とあまをで をえびどう
+
+ののろあじ ぞとろつけ
+びまぎぬめ めざひおね
+めだみこよ びやせれち
+いるたがを ふぜてこお
+げふぞそず みばてでむ
+
+ゆあはもべ おとどへぎ
+こむくげせ ゆしずつだ
+れちどにぶ なべぬよね
+めえみぜほ くやどめち
+するぼらつ にせずなど
+
+げけぎはず しやほごけ
+```
---
+++ src/main.rs
@@ -0,0 +1,222 @@
+use std::io::Read;
+use std::io::Write;
+use std::fs::File;
+
+enum SaveType {
+  English,
+  Japanese,
+}
+
+/// Golden Sun password converter (English -> Japanese, Japanese -> English)
+/// Hambaka <readeryoshi@gmail.com>
+/// Port Java version to Rust version with help from my friend "Flat Bartender", a huge thanks to him!
+fn main() {
+  let mut input_file = File::open("input.txt").expect("An error happened while opening input.txt");
+  let mut password = String::new();
+  input_file.read_to_string(&mut password).unwrap();
+
+  // If the file is empty, exit program.
+  if password.is_empty() {
+    println!("\"input.txt\" is empty!");
+    return;
+  }
+
+  // Get converted password.
+  // If input file is English version, then output file is Japanese version.
+  // If input file is Japanese version, then output file is English version.
+  let converted_password: String = match get_save_type(password.as_ref()) {
+    SaveType::English => password.chars().map(convert_en_to_jp).collect(),
+    SaveType::Japanese => password.chars().map(convert_jp_to_en).collect(),
+  };
+
+  // After converting, the result is "output.txt" in the same folder.
+  // If "output.txt" was already existed, clear all content in the file and write converted password.
+  // If not, create file and write converted password.
+  let mut output_file = std::fs::File::create("output.txt").expect("Create \"output.txt\" failed!");
+
+  output_file.write_all(converted_password.as_bytes()).expect("Write to \"output.txt\" failed!");
+
+  println!("Conversion is completed! Please check out \"output.txt\".");
+}
+
+/// Convert English version password (letters, numbers, signs) to Japanese version password (hiragana).
+fn convert_en_to_jp(input: char) -> char {
+  match input {
+    'A' => 'あ',
+    'B' => 'い',
+    'C' => 'う',
+    'D' => 'え',
+    'E' => 'お',
+
+    'F' => 'か',
+    'G' => 'き',
+    'H' => 'く',
+    'J' => 'け',
+    'K' => 'こ',
+
+    'L' => 'さ',
+    'M' => 'し',
+    'N' => 'す',
+    'P' => 'せ',
+    'Q' => 'そ',
+
+    'R' => 'た',
+
+    'S' => 'ち',
+    'T' => 'つ',
+    'U' => 'て',
+    'V' => 'と',
+    'W' => 'な',
+
+    'X' => 'に',
+    'Y' => 'ぬ',
+    'Z' => 'ね',
+    '2' => 'の',
+    '3' => 'は',
+
+    '4' => 'ひ',
+    '5' => 'ふ',
+    '6' => 'へ',
+    '7' => 'ほ',
+    '8' => 'ま',
+
+    '9' => 'み',
+
+    'a' => 'む',
+    'b' => 'め',
+    'c' => 'も',
+    'd' => 'や',
+    'e' => 'ゆ',
+
+    'f' => 'よ',
+    'g' => 'ら',
+    'h' => 'り',
+    'i' => 'る',
+    'j' => 'れ',
+
+    'k' => 'ろ',
+    'm' => 'わ',
+    'n' => 'ん',
+    'p' => 'を',
+    'q' => 'が',
+
+    'r' => 'ぎ',
+
+    's' => 'ぐ',
+    't' => 'げ',
+    'u' => 'ご',
+    'v' => 'ざ',
+    'w' => 'じ',
+
+    'x' => 'ず',
+    'y' => 'ぜ',
+    'z' => 'ぞ',
+    '!' => 'だ',
+    '?' => 'で',
+
+    '#' => 'ど',
+    '&' => 'ば',
+    '$' => 'び',
+    '%' => 'ぶ',
+    '+' => 'べ',
+
+    '=' => 'ぼ',
+    _ => input,
+  }
+}
+
+/// Convert Japanese version password (hiragana) to English version password (letters, numbers, signs).
+fn convert_jp_to_en(input: char) -> char {
+  match input {
+    'あ' => 'A',
+    'い' => 'B',
+    'う' => 'C',
+    'え' => 'D',
+    'お' => 'E',
+
+    'か' => 'F',
+    'き' => 'G',
+    'く' => 'H',
+    'け' => 'J',
+    'こ' => 'K',
+
+    'さ' => 'L',
+    'し' => 'M',
+    'す' => 'N',
+    'せ' => 'P',
+    'そ' => 'Q',
+
+    'た' => 'R',
+    'ち' => 'S',
+    'つ' => 'T',
+    'て' => 'U',
+    'と' => 'V',
+
+    'な' => 'W',
+    'に' => 'X',
+    'ぬ' => 'Y',
+    'ね' => 'Z',
+    'の' => '2',
+
+    'は' => '3',
+    'ひ' => '4',
+    'ふ' => '5',
+    'へ' => '6',
+    'ほ' => '7',
+
+    'ま' => '8',
+    'み' => '9',
+    'む' => 'a',
+    'め' => 'b',
+    'も' => 'c',
+
+    'や' => 'd',
+    'ゆ' => 'e',
+    'よ' => 'f',
+
+    'ら' => 'g',
+    'り' => 'h',
+    'る' => 'i',
+    'れ' => 'j',
+    'ろ' => 'k',
+
+    'わ' => 'm',
+    'を' => 'p',
+    'ん' => 'n',
+
+    'が' => 'q',
+    'ぎ' => 'r',
+    'ぐ' => 's',
+    'げ' => 't',
+    'ご' => 'u',
+
+    'ざ' => 'v',
+    'じ' => 'w',
+    'ず' => 'x',
+    'ぜ' => 'y',
+    'ぞ' => 'z',
+
+    'だ' => '!',
+    'で' => '?',
+    'ど' => '#',
+
+    'ば' => '&',
+    'び' => '$',
+    'ぶ' => '%',
+    'べ' => '+',
+    'ぼ' => '=',
+    _ => input,
+  }
+}
+
+/// Very stupid and lazy way to detect content (English version password or Japanese version password).
+/// By comparing first char to 'z' in ASCII. Because 'z' is the "largest" char in English version password.
+/// If the first char is larger than 'z', then it is Japanese version password, since Japanese version password are all hiragana.
+fn get_save_type(password: &str) -> SaveType {
+  let first_char = password.chars().nth(0).unwrap();
+  if first_char <= 'z' {
+    SaveType::English
+  } else {
+    SaveType::Japanese
+  }
+}
